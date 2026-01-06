aadfb741e38da2a26262d009a7ad3f82642dc1f3
nonnopomu <codebrew256@gmail.com>
2026-01-06 22:59:25 +0900

    lint

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,7 @@ Bundler.require(*Rails.groups)
 module App
   class Application < Rails::Application
     config.load_defaults 7.2
-    
+
     config.autoload_lib(ignore: %w[assets tasks])
 
     config.generators do |g|
